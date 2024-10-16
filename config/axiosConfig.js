e64ff6267d0581a95fda2067c753bbea4130894d
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-10-16 11:05:58 +0700

    style: home screen

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -1,18 +1,17 @@
-import { API } from 'api/apiUrl';
 import axios from 'axios';
 import { BASE_URL } from '../utils/environment';
 import storageMethod from '../utils/storageMethod';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  timeout: 10000,
 });
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
     // Do something before request is sent
-
-    config.headers.Authorization = `Bearer ${storageMethod.get().access_token}`;
+    config.headers.Authorization = `Bearer ${storageMethod.get().token}`;
     return config;
   },
   function (error) {
@@ -28,42 +27,8 @@ axiosInstance.interceptors.response.use(
     // Do something with response data
     return response.data;
   },
-  async (error) => {
+  (error) => {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
-    console.log('error', error);
-    const originalRequest = error.config;
-
-    // If error status is 403 or 401 and request has not key _retry
-    if (
-      (error.response?.status === 403 || error.response?.status === 401) &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
-      try {
-        // Call API refresh token for new token
-        const res = await axiosInstance.get(`${API.REFRESH_TOKEN}`, {
-          headers: {
-            Authorization: `Bearer ${storageMethod.get()?.refresh_token}`,
-          },
-        });
-
-        const { access_token, refresh_token } = res || {};
-        // Save new token to localStorage
-        storageMethod.set({ access_token, refresh_token });
-
-        // Change Authorization header
-        originalRequest.headers.Authorization = `Bearer ${access_token}`;
-
-        // Call API again with new token
-        return axiosInstance(originalRequest);
-      } catch (error) {
-        console.log(error);
-        // Handle when refresh token fail
-        storageMethod.remove();
-        return Promise.reject(error);
-      }
-    }
-
     // Do something with response error
     return Promise.reject(error);
   },
--- screens/HomeScreen/layout.js
+++ screens/HomeScreen/layout.js
@@ -1,15 +1,68 @@
-import { ImageBackground, View } from 'react-native';
+import { Button } from '@rneui/themed';
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <ImageBackground
-        className="flex-1"
-        source={require('./../../assets/images/Header_Information.png')}
-        resizeMode="cover"
-      />
-    </View>
+    <ScrollView style={styles.container}>
+      <View>
+        <Image
+          style={styles.image}
+          source={require('./../../assets/images/Header_Information.png')}
+        />
+      </View>
+      <View className="py-10 px-4">
+        <View>
+          <Text className="font-bold text-lg mb-3">Giới thiệu</Text>
+          <Text className="mb-3">
+            Hopeful Tails Trust được xây dựng dựa trên tiêu chí cổng thông tin
+            khổng lồ và hoàn toàn miễn phí về vấn đề xung quanh động vật, đặc
+            biệt là thú cưng, cụ thể là vấn đề thú cưng bị bỏ rơi, các thực phẩm
+            dinh dưỡng dành cho thú cưng, cách chăm sóc và điều trị bệnh cho chó
+            mèo,... Tất cả những thông tin tại Hopeful Tails Trust được sưu tầm
+            và tham khảo từ nhiều nguồn khác nhau có chọn lọc, bên cạnh đó còn
+            là trải nghiệm thực tế của chúng tôi trong quá trình nuôi thúc cưng
+            để đem đến cho mọi người những thông tin chính xác và hữu ích nhất.
+          </Text>
+          <Pressable style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}>
+            <Button size="md">Medium</Button>
+          </Pressable>
+        </View>
+        <View className="overflow-hidden rounded-2xl mt-3">
+          <Image
+            className="object-cover w-96 h-72"
+            resizeMode="cover"
+            source={require('../../assets/images/cat-sleep.png')}
+          />
+        </View>
+      </View>
+      <View>
+        <Text>Blog and News</Text> 
+        <View></View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default HomeScreen;
+
+const windowWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: windowWidth * 1,
+    height: windowWidth * 0.8 * (9 / 16),
+    resizeMode: 'cover',
+  },
+});
